96bd7e4138dce963703151061bf8c398d521b82d
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-09-18 20:33:39 -0400

    aj's js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+var url = `https://api.nytimes.com/svc/search/v2/articlesearch.json`
 
+url += "?" + $.param({
+    "api-key": "####",
+    "q": "####"
+});
+$.ajax({
+    url: url;
+    method: "GET",
+}).done(function(result){
+    console.log(result);
+}).fail(function(err) {
+    throw err;
+});
