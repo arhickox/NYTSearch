d07dffef95d8344ba9d56db8d99b405a77518411
Kelvin Jones <kevinjones4453@gmail.com>
2018-09-18 21:22:33 -0400

    wip:1.0

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 var url = `https://api.nytimes.com/svc/search/v2/articlesearch.json`
 
 url += "?" + $.param({
-    "api-key": "####",
-    "q": "####"
+    "api-key": "570821d2229143f680c8280a52df84d9",
+    "q": 'president',
+    "page": 3,
+    "begin_date": "20160808",
+    "end_date": "20180808"
 });
 $.ajax({
-    url: url;
+    url: url,
     method: "GET",
 }).done(function(result){
     console.log(result);
+    console.log()
 }).fail(function(err) {
     throw err;
 });
