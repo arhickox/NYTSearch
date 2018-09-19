269f3200529dd30880f051b53911bd7b4e235998
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-09-18 21:22:06 -0400

    some of it works!

--- main.js
+++ main.js
@@ -2,17 +2,24 @@ var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
 
 url += "?" + $.param({
     "api-key": "0a94c61548784861b5488da2c9caaa10",
-    "q": $("#searchTerm").val(),
-    "page" : $("#recordsRetrieved").val(),
-    "begin_date": $("#beginDate").val(),
-    "end_date": $("#endDate").val(),
+    "q": "president",
+    //"page" : $("#recordsRetrieved").val(),
+   // "begin_date": $("#beginDate").val(),
+   // "end_date": $("#endDate").val(),
 });
 $.ajax({
     url: url,
     method: "GET",
-}).done(function(result){
+}).then(function(response){
+    console.log(response);
+    const result = response.response.docs;
     console.log(result);
-}).fail(function(err) {
-    throw err;
-});
 
+for (var i = 0; i < 5; i++) {
+    const title = result[i].headline.main;
+    const publishDate = result[i].pub_date;
+    const snip = result[i].snippet;
+    console.log(title);
+    console.log(publishDate);
+    console.log(snip);
+}});
