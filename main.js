cd11ba31b3afa02488ab02fc84547f1eccb4164e
Kelvin Jones <kevinjones4453@gmail.com>
2018-09-19 14:54:13 -0400

    modified main.js & main.html to display response

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+let searchKey = '';
+let previous = '';
+let records;
+let page = 0;
 
 $('document').ready(function () {
 
     $('#searchBtn').on('click', function () {
+        searchKey = $('#searchTerm').val().trim()   // grabs the user's search term
+        records = $('#records').val();
+
+        /*if (previous === searchKey) {
+            page++;
+        }*/
 
         url += "?" + $.param({
             "api-key": "570821d2229143f680c8280a52df84d9",
-            "q": $('#searchTerm').val().trim(),         // grabs the user's search term
-            "page" : 1,                                 // grabs the selected page range
+            "q": searchKey,         
+            "page" : page,  
             "begin_date": $('#beginDate').val().trim(), // grabs the begin date from the form
             "end_date": $('#endDate').val().trim(),     // grabs the end date from the form
         });
@@ -19,12 +29,19 @@ $('document').ready(function () {
             method: "GET",      // ajax GET method requests data from the nytimes API
         }).done(function(result){
             console.log(result);    // log the response for debugging
+            console.log('records ' + records);
             myResult = result;
+            $('#resultCard').empty();
             createElements(myResult);   // calls the createElements function to display the articles
         }).fail(function(err) {
             throw err;
         }); 
         
+        //previous = searchKey;
+    });
+
+    $('#clearBtn').on("click", function (){
+        $('#myForm')[0].reset();        // reset the fields
     });
     
 
@@ -33,7 +50,7 @@ $('document').ready(function () {
 function createElements (result) {
     let newDiv = $("<div>");        // create a new div for the response elements
 
-    for (let x = 0; x < result.response.docs.length; x++ ) {    // loop through all objects and display 
+    for (let x = 0; x < records; x++ ) {    // loop through all objects and display 
         newDiv.append(`<h3>${result.response.docs[x].headline.main}</h3>
             <p>${result.response.docs[x].byline.original}</p>
             <p>${result.response.docs[x].snippet}</p>
@@ -41,5 +58,6 @@ function createElements (result) {
         $('#resultCard').append(newDiv);
     }
 }
+
 
 
